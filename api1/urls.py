67c578f72d8b95d612fe6a99f5881aa2033ffe01
FabianBustos <nottfenrir@gmail.com>
2023-10-12 12:51:17 -0300

    prueba de filtros pasada, todo funciona bien hasta ahora

--- api1/urls.py
+++ api1/urls.py
@@ -4,7 +4,13 @@ from rest_framework import routers
 from .views import CatalogoViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api/catalogo',views.CatalogoViewSet, 'catalogo')
+router.register(r'catalogo',views.CatalogoViewSet, 'catalogo')
 
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    # path('api/catalogo/<str:tematica>/', TuViewSet.as_view({'get': 'retrieve'}), name='tu-recurso-detail'),
+    path('a/',views.catalogobytematica)
+]
--- api1/views.py
+++ api1/views.py
@@ -2,12 +2,28 @@ from rest_framework import viewsets
 from .serializers import CatalogoSerializer
 from .models import Catalogo2
 
+from django.http import HttpResponse
+
+
 
 class CatalogoViewSet(viewsets.ModelViewSet):
     queryset = Catalogo2.objects.all()
     serializer_class = CatalogoSerializer
 
+# class CatalogoByTematicaViewSet(viewsets.ModelViewSet,):
+#     queryset = Catalogo2.objects.filter(tematica__icontains=)
 
+
+def catalogobytematica(request):
+    busqueda = 'da'
+    objetos = Catalogo2.objects.filter(tematica__icontains=busqueda)
+    i = 0
+    for obj in objetos:
+        i += 1
+    print(i)
+    return HttpResponse('hola')
+    
+    
 
 
 
